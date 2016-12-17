f79d3e66e5ead33bce3634874fe675c8da901eee
yuoku <yuoku0708@gmail.com>
2016-12-17 13:04:27 +0900

    Initial Commit

---
+++ FlickrTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrTest
+//
+//  Created by OkuderaYuki on 2016/12/17.
+//  Copyright © 2016年 YukiOkudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
